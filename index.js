7430355d330a1df304b6a6eefab6ca904821b7ea
unknown <laura@fliegner.net>
2020-06-22 23:08:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var likes = 0;
+
+function onClick() {
+  likes += 1;
+  document.getElementById("likes").innerHTML = likes;
+}
+
+var likes1 = 0;
+
+function onClick() {
+  likes1 += 1;
+  document.getElementById("likes1").innerHTML = likes1;
+}
