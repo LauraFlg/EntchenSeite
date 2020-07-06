21195dd972c8de0f8697bc1a7104d08dd29e6f0b
unknown <laura@fliegner.net>
2020-07-06 19:14:43 +0200

    adds functioning dislikebuttons

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ function onClick() {
   document.getElementById("likes").innerHTML = likes;
 }
 
+function onClickDislike() {
+  likes -= 1;
+  document.getElementById("likes").innerHTML = likes;
+}
+
 var likesSecond = 0;
 function onClick1() {
   likesSecond += 1;
+  document.getElementById("likesSecond").innerHTML = likesSecond;
+}
+
+function onClickDislike1() {
+  likesSecond -= 1;
   document.getElementById("likesSecond").innerHTML = likesSecond;
 }
